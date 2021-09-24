544127177fc82be76e99022870eb0a6f38d732a9
mlondong <mauricio.londono@gmail.com>
2021-09-23 19:31:39 -0500

    added CorsFilter bean

--- src/main/java/co/com/foodbank/vault/Application.java
+++ src/main/java/co/com/foodbank/vault/Application.java
@@ -1,10 +1,15 @@
 package co.com.foodbank.vault;
 
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import co.com.foodbank.contribution.sdk.config.EnableContributionSDK;
 import co.com.foodbank.user.sdk.config.EnableUserSDK;
 
@@ -17,6 +22,30 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration
+                .setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin",
+                "Access-Control-Allow-Origin", "Content-Type", "Accept",
+                "Authorization", "Origin, Accept", "X-Requested-With",
+                "Access-Control-Request-Method",
+                "Access-Control-Request-Headers"));
+        corsConfiguration.setExposedHeaders(Arrays.asList("Origin",
+                "Content-Type", "Accept", "Authorization",
+                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin",
+                "Access-Control-Allow-Credentials"));
+        corsConfiguration.setAllowedMethods(
+                Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource =
+                new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",
+                corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
     }
 
 }
--- src/main/java/co/com/foodbank/vault/restcontroller/VaultRestController.java
+++ src/main/java/co/com/foodbank/vault/restcontroller/VaultRestController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,6 +45,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
  *         31/05/2021
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(value = "/vault")
 @Tag(name = "Vault", description = "the vault API")
 @Validated
